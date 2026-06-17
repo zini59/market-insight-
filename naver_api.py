919bbeb2127e23ea4577fe911df65779f5d0b2cd
zini59 <yegenie59@gmail.com>
2026-06-18 01:42:40 +0900

    Add files via upload

--- naver_api.py
+++ naver_api.py
@@ -1,7 +1,8 @@
 import requests
 
-NAVER_ID = "YOUR_CLIENT_ID"
-NAVER_SECRET = "YOUR_CLIENT_SECRET"
+NAVER_ID = "IcKmtQv41oAWPQDGUhNT"
+NAVER_SECRET = "NUbkh9S03S"
+
 
 def naver_place_search(query):
     url = "https://openapi.naver.com/v1/search/local.json"
@@ -18,5 +19,9 @@ def naver_place_search(query):
     }
 
     res = requests.get(url, headers=headers, params=params)
+
+    # 오류 체크 (중요)
+    if res.status_code != 200:
+        return {"error": res.text}
 
     return res.json()
